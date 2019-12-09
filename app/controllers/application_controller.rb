155ad73cf274fc8881ae0b7fc2fc3173fc5d2595
Chaya Deaver <chayadeaver@gmail.com>
2019-12-08 19:55:38 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -8,6 +9,41 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles/new' do
+    erb :new
   end
+
+  post '/articles' do
+    article = Article.create(params)
+    # binding.pry
+    redirect "articles/#{article.id}"
+    
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    # binding.pry
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by(id: params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    # binding.pry
+    params.delete("_method")
+    @article.update(params)
+    redirect "articles/#{article.id}"
+  end
+
+  
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+    def change
+        create_table :articles do |a|
+            a.string :title
+            a.string :content
+        end
+    end
 end
